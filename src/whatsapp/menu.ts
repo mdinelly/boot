02b6694e0076903cd755dbb320d3cb8c8dfaad10
mdinelly <37814470+mdinelly@users.noreply.github.com>
2022-10-27 20:13:37 -0300

    Create menu.ts
    Inicial

---
+++ src/whatsapp/menu.ts
@@ -0,0 +1,77 @@
+
+const languagemenu: any =   {
+  pt: [
+    {
+      id: 1,
+      descricao: 'Localizar dispositivo ',
+    },
+    {
+      id: 2,
+      descricao: 'Localizar dispositivo desligado ou offline ',
+    },
+    {
+      id: 3,
+      descricao: 'Informar o protocolo manualmente',
+    },
+    {
+      id: 4,
+      descricao: 'Encerrar atendimento',
+    }
+  ],
+  en: [
+    {
+      id: 1,
+      descricao: 'Find Device',
+    },
+    {
+      id: 2,
+      descricao: 'Find device offline or offline ',
+    },
+    {
+      id: 3,
+      descricao: 'Enter the protocol manually',
+    },
+    {
+      id: 4,
+      descricao: 'End service',
+    }
+  ],
+  fr: [
+    {
+      id: 1,
+      descricao: 'Trouver un appareil',
+    },
+    {
+      id: 2,
+      descricao: 'Rechercher un appareil hors ligne ou hors ligne ',
+    },
+    {
+      id: 3,
+      descricao: 'Entrez le protocole manuellement',
+    },
+    {
+      id: 4,
+      descricao: 'Fin de service',
+    }
+  ],
+  es: [
+    {
+      id: 1,
+      descricao: 'Buscar dispositivo',
+    },
+    {
+      id: 2,
+      descricao: 'Buscar dispositivo sin conexión o sin conexión',
+    },
+    {
+      id: 3,
+      descricao: 'Ingrese el protocolo manualmente',
+    },
+    {
+      id: 4,
+      descricao: 'Servicio final',
+    }
+  ],
+}
+
+export default languagemenu;
