ba9a1cd2a6ee8f84e92dccecb1734b4dad4f5139
mdinelly <37814470+mdinelly@users.noreply.github.com>
2022-10-27 20:13:33 -0300

    Create stages.ts
    Inicial

---
+++ src/whatsapp/bots/stages.ts
@@ -0,0 +1,25 @@
+// const stageLocalizarrDispostivo = (
+//   opcaoSelecionada: any,
+//   name: any,
+//   model: any,
+//   link: any
+// ) => {
+//   return `*🤖 Suporte Oficial da Apple*\nOpção ( ${opcaoSelecionada} ) selecionada com sucesso.\n\nPrezado(a) ${name},\nO seu *${model}* emitiu uma nova localização.\nVeja em: ${link}. Logo após você será redirecionado para a *Página de Suporte Oficial da Apple.* Você deve preencher o seu formulário de recuperação, para que possamos lhe encaminhar para uma de nossas lojas mais próxima de você.\n\nAtenciosamente,\n*🤖 Suporte Oficial da Apple*\n\nDigite * para encerrar o atendimento.`;
+// };
+
+// const stageLocalizarrDispostivoDesliadoOuOffline = (
+//   opcaoSelecionada: any,
+//   link: any
+// ) => {
+//   return `*🤖 Suporte Oficial da Apple*\nOpção ( ${opcaoSelecionada} ) selecionada com sucesso.\n\n*iPhone Localizável Após Desligado*\nO seu iPhone foi emparelhado via Bluetooth a um outro Dispositivo Apple e emitiu a sua última localização. Acesse: ${link} e veja a sua última localização na Rede do App Buscar antes que ela expire.\n\nAtenciosamente,\n*🤖 Suporte Oficial da Apple *\n\nDigite * para encerrar o atendimento.`
+// };
+
+// const informarProtocoloManualmente = (opcaoSelecionada: any) => {
+//   return `*🤖 Suporte Oficial da Apple*\nOpção ( ${opcaoSelecionada} ) selecionada com sucesso.\n\nPor favor, digite o último protocolo recebido por SMS ou Atendimento:\n\nDigite * para encerrar o atendimento.`
+// };
+
+// const encerrarAtendimento = () => {
+//   return `Assistente Virtual\nAtendimento encerrado com sucesso, Obrigado. 😃\n\n\nEspero ter conseguido te ajudar, a Apple agradeçe o seu contato, até mais. 👋\n\n\nPara mais informações ligue para 0800 201 7581.`
+// };
+
+// export {stageLocalizarrDispostivo, stageLocalizarrDispostivoDesliadoOuOffline, informarProtocoloManualmente, encerrarAtendimento};
